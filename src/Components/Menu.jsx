27333c63e6537fcae27c14213057c7e20a2ed311
ParveenPasricha <pasricha.parveen@yahoo.in>
2025-01-31 12:51:38 +0530

    Fix: fix layout

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -35,16 +35,16 @@ const Menu = () => {
       <div ref={menuRef} className="flex items-center flex-col sm:flex-row">
         {/* Hamburger Menu Icon */}
         <IoMdMenu
-          className="hover:text-purple-400 text-2xl lg:hidden"
+          className="hover:text-purple-400 text-2xl lg:hidden md:hidden"
           onClick={toggleMenu}
         />
 
         {/* Menu Items */}
-        <div className={`flex items-center md:flex-col sm:flex-row gap-5 sm:gap-20 ${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
-          <Link to="/" className="hover:text-purple-400">Home</Link>
-          <Link to="/project" className="hover:text-purple-400">Project</Link>
-          <Link to="/blog" className="hover:text-purple-400">Blog</Link>
-          <Link to="/contact" className="hover:text-purple-400">Contact</Link>
+        <div className={`flex items-center md:flex-col sm:flex-row gap-5 sm:gap-20  ${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
+          <Link to="/" className="hover:text-purple-400 lg:mr-2">Home</Link>
+          <Link to="/project" className="hover:text-purple-400 lg:mr-2">Project</Link>
+          <Link to="/blog" className="hover:text-purple-400 lg:mr-2">Blog</Link>
+          <Link to="/contact" className="hover:text-purple-400 lg:mr-2">Contact</Link>
           <Link className="bg-purple-600 font-bold text-white rounded-lg text-xl hover:bg-green-600" to="/hire">Hire Me</Link>
         </div>
       </div>
