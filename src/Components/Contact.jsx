48a1fc86a25e5227150e2b73bb8856b80dd36596
Parveen Pasricha <pasricha.parveen@yahoo.in>
2025-07-22 12:04:49 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,18 +3,22 @@ import { FaCloudDownloadAlt } from "react-icons/fa";
 
 const Contact = () => {
   function Download() {
-    const DownloadFile =
-    "https://drive.google.com/uc?export=download&id=1Op4TVlbvRPq-vgZYAT2uqzHkeOuhksX-"
+    const downloadFile =
+      "https://docs.google.com/uc?export=download&id=1eVoNsLCBVwrokIs02Ect1X3PGoBOLEVy";
 
-    console.log(DownloadFile);
-    window.location.href = DownloadFile;
+    console.log("Downloading from:", downloadFile);
+    window.location.href = downloadFile;
   }
- 
+
   return (
     <div className="items-center justify-center text-3xl flex flex-col px-4 sm:px-8 md:px-12">
       <label className="m-6">Download My CV</label>
-      <button className="text-3xl px-48 bg-yellow-500 w-96" onClick={Download}>
+      <button
+        className="text-3xl px-48 py-4 bg-yellow-500 hover:bg-yellow-600 text-white rounded-md transition duration-300 w-96 flex items-center justify-center gap-3"
+        onClick={Download}
+      >
         <FaCloudDownloadAlt />
+        Download
       </button>
     </div>
   );
