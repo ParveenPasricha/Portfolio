78bc78a14d82ab3b30202df24a159c0e9dc3d98c
Parveen Pasricha <pasricha.parveen@yahoo.in>
2025-07-22 12:10:07 +0530

    Update Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,7 +4,7 @@ import { FaCloudDownloadAlt } from "react-icons/fa";
 const Contact = () => {
   function Download() {
     const downloadFile =
-      "https://docs.google.com/uc?export=download&id=1eVoNsLCBVwrokIs02Ect1X3PGoBOLEVy";
+      "https://docs.google.com/document/d/1eVoNsLCBVwrokIs02Ect1X3PGoBOLEVy/edit?usp=sharing&ouid=115416944460699501084&rtpof=true&sd=true";
 
     console.log("Downloading from:", downloadFile);
     window.location.href = downloadFile;
